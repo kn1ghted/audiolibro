b563e9ddf61a27cf1229dff7e1ebd6806e21fce0
Ed <edca81@gmail.com>
2024-09-22 20:21:03 -0600

    gh-pages
    gh-pages npm package and public folder setup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
