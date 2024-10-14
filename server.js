e75066b789df269b060724ab93931c0dd9252623
Ed <edca81@gmail.com>
2024-10-13 20:03:59 -0600

    internal links
    public folder links updated to work on github pages

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Add a prefix to all routes under /api
+const ghRouter = express.Router();
+
 // use static files
 app.use(express.static(__dirname + '/public'));
 // Serve static files from 'pages' directories
@@ -13,6 +16,15 @@ app.use(express.static(__dirname + '/public/pages/'));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// Example routes under the /api prefix
+ghRouter.get('/example/:number', (req, res) => {
+  const exampleNumber = req.params.number;
+  const filePath = path.join(__dirname, '/public/pages/', `example-${exampleNumber}.html`);
+});
+
+// Prefixing the routes using app.use()
+app.use('https://kn1ghted.github.io/audiolibro/', ghRouter);
 
 // Define a route to catch URLs like /example/1, /example/2, etc.
 app.get('/example/:number', (req, res) => {
