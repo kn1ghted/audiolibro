8cee1eb88e2aa947294f960b290ca6752f847449
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2024-11-09 23:33:34 -0600

    Add audio player component and refactor page and section components; update HTML structure.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,23 +1,35 @@
 import AudioPlayer from "./waveform.jsx";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
 export const Section = ({ section }) => {
+  const location = useLocation();
 
-    switch (section.type) {
-        case "header":
-            return (
-                <h2 className="text-2xl font-bold mt-5 mb-3">{section.content}</h2>
-            );
+  console.log(location.pathname);
 
-        case "text":
-            return <p className="mb-4 text-gray-700">{section.content}</p>;
+  switch (section.type) {
+    case "header":
+      return (
+        <h2 className="text-2xl font-bold mt-5 mb-3">{section.content}</h2>
+      );
 
-        case "audio": {
-            
-            return <AudioPlayer data={section.sources} />;
-        }
-        default:
-            return null;
+    case "text":
+      return <p className="mb-4 text-gray-700">{section.content}</p>;
+
+    case "audio": {
+      return (
+        <AudioPlayer key={location.pathname} playlists={section.playlists} />
+      );
+    }
+    default:
+      return null;
+  }
 };
 
+Section.propTypes = {
+  section: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    playlists: PropTypes.array,
+  }).isRequired,
 };
--- src/data/pages.js
+++ src/data/pages.js
@@ -16,22 +16,48 @@ const pages = [
         {
           type: "audio",
           content: "audio",
-          idPayer: "quinta-completa",
-          sources: [
+          playlists: [
             {
-              src: "src/assets/audio/kalote-5/01-quinta_clave_completa/00-click-198bpm.wav",
-              name: "Click",
-              type: "audio/wav",
-            },
-            {
-              src: "src/assets/audio/kalote-5/01-quinta_clave_completa/01-clave.wav",
-              name: "Clave",
-              type: "audio/wav",
-            },
-            {
-              src: "src/assets/audio/kalote-5/01-quinta_clave_completa/02-cata.wav",
-              name: "Cata",
-              type: "audio/wav",
+              id: "quinta-completa",
+              data: [
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/01-clave.wav",
+                  name: "Clave",
+                  loop: true,
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/02-cata.wav",
+                  name: "Cata",
+                  loop: true,
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/03-chequere.wav",
+                  name: "Chequere",
+                  loop: true,
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/04-quinto.wav",
+                  name: "Quinto",
+                  loop: true,
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/05-conga.wav",
+                  name: "Conga",
+                  loop: true,
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/06-tumba.wav",
+                  name: "Tumba",
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/07-campana.wav",
+                  name: "Campana",
+                },
+                {
+                  src: "src/assets/audio/convite-7/01-clave-completa/08-campana_mano.wav",
+                  name: "Campana a mano",
+                }
+              ],
             },
           ],
         },
@@ -42,6 +68,46 @@ const pages = [
         {
           type: "text",
           content: "Variante con click a tempo 225 bpm.",
+        },
+        {
+          type: "audio",
+          content: "audio",
+          idPlayer: "quinta-completa-rapida",
+          playlists: [
+            {
+              id: "quinta-completa-rapida",
+              data: [
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/01-clave.wav",
+                  name: "Clave",
+                },
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/02-cata.wav",
+                  name: "Cata",
+                },
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/03-chequere.wav",
+                  name: "Chequere",
+                },
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/04-conga.wav",
+                  name: "Conga",
+                },
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/05-tumba.wav",
+                  name: "Tumba",
+                },
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/06-campana.wav",
+                  name: "Campana",
+                },
+                {
+                  src: "src/assets/audio/convite-7/02-clave-completa_rapida/07-campana_mano.wav",
+                  name: "Campana a mano",
+                }
+              ],
+            },
+          ],
         },
       ],
     },
@@ -63,11 +129,39 @@ const pages = [
         {
           type: "audio",
           content: "audio",
-          idPayer: "quinta-abierta",
-          sources: [
+          playlists: [
             {
-              src: "src/assets/audio/mix.wav",
-              type: "audio/wav",
+              id: "qca-1",
+              data: [
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/01-clave.wav",
+                  name: "Clave",
+                },
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/02-chequere.wav",
+                  name: "Chequere",
+                },
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/03-chequere_golpe.wav",
+                  name: "Chequere golpe",
+                },
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/04-conga.wav",
+                  name: "Conga",
+                },
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/05-tumba.wav",
+                  name: "Tumba",
+                },
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/06-tumba_variacion.wav",
+                  name: "Tumba variación",
+                },
+                {
+                  src: "src/assets/audio/convite-7/03-clave-abierta/07-campana_mano.wav",
+                  name: "Campana a mano",
+                }
+              ],
             },
           ],
         },
@@ -78,18 +172,6 @@ const pages = [
         {
           type: "text",
           content: "Variante con click a 225 bpm.",
-        },
-        {
-          type: "audio",
-          content: "audio",
-          idPayer: "quinta-abierta-rapida",
-          sources: [
-            {
-              src: "src/assets/audio/mix.wav",
-              name:"Mix",
-              type: "audio/wav",
-            },
-          ],
         },
       ],
     },
@@ -108,18 +190,6 @@ const pages = [
           content:
             "Se subdividió el patrón del Requinto (Tambor Chico) en cinco grupos de cuatro corcheas cada uno , donde se acentúa la segunda corchea (golpe abierto con la mano que no tiene baqueta), respetando el patrón del Tambor Chico original que consta de cuatro grupos de cuatro corcheas cada uno.",
         },
-        {
-          type: "audio",
-          content: "audio",
-          idPayer: "quinto-5-4",
-          sources: [
-            {
-              src: "src/assets/audio/mix.wav",
-              name:"Mix",
-              type: "audio/wav",
-            },
-          ],
-        },
       ],
     },
   },
@@ -137,28 +207,7 @@ const pages = [
           content:
             "Para la creación de los ritmos en 5 fue muy importante el desarrollo de distintas claves compuestas que le dieran sentido y cohesión a estos ritmos; para ello se crearon 7 claves compuestas y un patrón rítmico en Timbal que cumple función de clave.",
         },
-        {
-          type: "audio",
-          content: "audio",
-          idPayer: "completa-7",
-          sources: [
-            {
-              src: "src/assets/audio/mix.wav",
-              name:"Mix",
-              type: "audio/wav",
-            },
-            {
-              src: "src/assets/audio/mix.wav",
-              name:"Mix",
-              type: "audio/wav",
-            },
-            {
-              src: "src/assets/audio/mix.wav",
-              name:"Mix",
-              type: "audio/wav",
-            },
-          ],
-        },
+
       ],
     },
   },
